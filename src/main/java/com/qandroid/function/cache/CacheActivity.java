4d45d10f6058a1c34c3c5f343c9f968f059130ec
chengxiang.peng <chengxiang.peng@qunar.com>
2017-01-06 22:28:50 +0800

    修改成本地变量

--- src/main/java/com/qandroid/function/cache/CacheActivity.java
+++ src/main/java/com/qandroid/function/cache/CacheActivity.java
@@ -11,22 +11,20 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class CacheActivity extends AppCompatActivity{
-    private GridView gridView;
-    private ImageListAdapter imageListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cache);
 
-        gridView = (GridView) findViewById(R.id.gridview1);
+        GridView gridView = (GridView) findViewById(R.id.gridview1);
         ArrayList<String> imageUrlList = new ArrayList<>();
         Random random = new Random();
         for (int i = 0; i < 40; i++) {
             imageUrlList.add("http://192.168.1.105:8080/qserver/img/" + random.nextInt(16) % 6 + ".jpg");
 
         }
-        imageListAdapter = new ImageListAdapter(this, imageUrlList);
+        ImageListAdapter imageListAdapter = new ImageListAdapter(this, imageUrlList);
         gridView.setAdapter(imageListAdapter);
     }
 }
